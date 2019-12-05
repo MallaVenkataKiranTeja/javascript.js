05364bec8892ed1de0c3e7d5ac8508c267af96ee
venkata kiran teja <kiranteja945@gmail.com>
2019-12-05 18:47:30 +0530

    hey commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeColorToRed() {
+    var heading = document.getElementById("heading_resume");
+    heading.style.color = "blue";
+}
+function displaymyfbprofile(){
+    var facebook = document.getElementById("heading_resume");
+    facebook.innerText = " href=\"https://www.facebook.com/profile.php?id=100006826186604";
+}
+function color() {
+    var name = document.getElementById("my_name");
+    name.style.color = "Green";
+}
+function text() {
+    var text =document.getElementById("text_color");
+    text.style.color = "red";
+}
